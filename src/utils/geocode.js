88ce394eb633532fc4276a23dfeb003864042c16
Forrest Naylor <Forrest@Forrests-MacBook-Pro.local>
2020-08-28 10:29:13 -0600

    geocode utility created

---
+++ src/utils/geocode.js
@@ -0,0 +1,27 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoiNGVzdG5heWxvciIsImEiOiJja2R6OGUzeGkzbDdrMnpxcXpnamp5bG1xIn0.g2BT8g3amegy_kObN5hvFw";
+
+  request({ url: url, json: true }, (error, { body } = {}) => {
+    if (error) {
+      callback("could not connect to geocode service", undefined);
+    } else if (body.features.length === 0) {
+      callback("could not find location, try another search.", undefined);
+    } else {
+      callback(undefined, {
+        location: body.features[0].place_name,
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+      });
+    }
+  });
+};
+
+geocode("Boston", (geoError, geoData) => {
+  console.log(geoError);
+  console.log(geoData);
+});
