df1cb328e3ec818adcebb42fe9d8be81e89a1a49
Forrest Naylor <Forrest@Forrests-MacBook-Pro.local>
2020-08-28 12:11:11 -0600

    hbs pages rendered

--- src/app.js
+++ src/app.js
@@ -1,8 +1,54 @@
-const geocode = require("./utils/geocode");
-const forecast = require("./utils/forecast");
+const path = require("path");
 
 const express = require("express");
 const hbs = require("hbs");
-const hbs_utils = require("hbs-utils");
+const hbsutils = require("hbs-utils")(hbs);
+
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
+const port = 3000;
+
+const publicRoute = path.join(__dirname, "../public");
+const viewsRoute = path.join(__dirname, "../templates/views/");
+const partialsRoute = path.join(__dirname, "../templates/partials/");
+
+app.set("view engine", "hbs");
+app.set("views", viewsRoute);
+app.use(express.static(publicRoute));
+
+hbs.registerPartials(partialsRoute);
+hbsutils.registerWatchedPartials(partialsRoute);
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Forrest Naylor",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Forrest Naylor",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Forrest Naylor",
+  });
+});
+
+app.get("/*", (req, res) => {
+  res.render("404", {
+    title: "404 Error",
+    name: "Forrest Naylor",
+  });
+});
+
+app.listen(port, () => {
+  console.log("server is up on port 3000");
+});
